6df703b84b6f07ceceb9f7b6f9c876a4d78bb290
khandragon <saaadkhan23@yahoo.ca>
2018-10-06 01:39:29 -0400

    establish a connection between server and client

--- connectFourClients/src/main/java/com/connectfour/controller/connectServer.java
+++ connectFourClients/src/main/java/com/connectfour/controller/connectServer.java
@@ -1,19 +1,25 @@
 package main.java.com.connectfour.controller;
 
+import java.io.DataInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.SocketException;
 import java.net.UnknownHostException;
 
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
 public class connectServer {
 
@@ -26,11 +32,45 @@ public class connectServer {
     @FXML
     private Button connectButton;
 
-    public void establishConnection(ActionEvent actionEvent){
+    public void establishConnection(ActionEvent actionEvent) {
         String address = inputAddress.getText();
         String port = inputPort.getText();
-        System.out.println(address);
-        System.out.println(port);
+        if (validInput(address, port))
+            connectToServer(address, port);
+    }
+
+    private void connectToServer(String server, String servPort) {
+        final Socket socket;
+        final DataInputStream in;
+        final OutputStream out;
+
+        try {
+            // Create socket and fetch I/O streams
+            socket = new Socket(server, Integer.parseInt(servPort));
+
+            in = new DataInputStream(socket.getInputStream());
+            out = socket.getOutputStream();
+
+            Stage stage = (Stage) closeButton.getScene().getWindow();
+
+            stage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+                @Override
+                public void handle(WindowEvent t) {
+                    try {
+                        socket.close();
+                    } catch (Exception exception) {
+                    }
+                    System.exit(0);
+                }
+            });
+
+        } catch (SocketException e) {
+        } catch (IOException e) {
+        }
+    }
+
+    private boolean validInput(String address, String port) {
+        return !address.isEmpty() && !port.isEmpty() && !address.equals(" ") && !port.equals(" ");
     }
 
     public void closeWindow(ActionEvent actionEvent) {
